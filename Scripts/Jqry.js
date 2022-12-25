7801071995ccd8d5967352c46d265e685831a608
JAY S LONDHE <77278361+Jaylondhe13@users.noreply.github.com>
2022-12-25 20:59:28 +0530

    Update Jqry.js

--- Scripts/Jqry.js
+++ Scripts/Jqry.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 $(".faq .accordion").click(function () {
   $(this).next().slideToggle("slow");
 
@@ -15,58 +8,5 @@ $(".faq .accordion").click(function () {
 
 
 
-
-
-// $('.newProduct img').addClass('hide')
-
-
-
-// $('.card> .Prod-hover').mouseover(function () {
-//   $(this).addClass('Blur1');
-//   $('.Blur1 + .newProduct img').removeClass('hide')
-
-
-// })
-
-
-
-// $('.col').mouseleave(function () {
-
-//   $('.Blur1 + .newProduct img').addClass('hide')
-//   $('.card> .Prod-hover').removeClass('Blur1')
-
-
-// })
-
-
-// $('.wishlist',).mouseover(function () {
-//   $(this).attr('src', 'img\\h1.png')
-
-// })
-
-// $('.eye').mouseover(function () {
-//   $(this).attr('src', 'img\\h2.png')
-
-// })
-
-// $('.basket').mouseover(function () {
-//   $(this).attr('src', 'img\\h3.png')
-
-// })
-
-// $('.wishlist').mouseout(function () {
-//   $(this).attr('src', 'img\\Group 2579.png')
-// })
-
-
-// $('.eye').mouseout(function () {
-//   $(this).attr('src', 'img\\Group 2580.png')
-// })
-
-// $('.basket').mouseout(function () {
-//   $(this).attr('src', 'img\\Group 2581.png')
-
-
-// })
 
 
