d19ada429985d03255dd0bfdaae0a9c87f38747a
JAY S LONDHE <77278361+Jaylondhe13@users.noreply.github.com>
2022-12-25 20:58:29 +0530

    Delete vendor.js

--- Scripts/vendor.js
+++
@@ -1,8 +0,0 @@
-// let country = document.getElementById('country');
-// let num = document.getElementById('num');
-// let flag = document.getElementById('flag');
-
-let getData = document.querySelector('.getData');
-
-
-  getData.innerHTML = 'jay'
